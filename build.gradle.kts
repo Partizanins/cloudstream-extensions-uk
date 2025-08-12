e538b0e570eb2e3f44e3d838618fcad972f6faa0
Partizanin <topiciv141@gmail.com>
2025-08-12 13:02:22 +0300

    downgrade lib version

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ subprojects {
         val implementation by configurations
 
         // Stubs for all cloudstream classes
-        cloudstream("com.lagradost:cloudstream3:452")
+        cloudstream("com.lagradost:cloudstream3:4.5.2")
 
         // These dependencies can include any of those which are added by the app,
         // but you don't need to include any of them if you don't need them.
